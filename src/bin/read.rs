cbc29277438fbb74a6f035dea973569d6f649c65
sadikovi <ivan.sadikov@gmail.com>
2018-02-22 23:43:04 +1300

    add record api

--- src/bin/read.rs
+++ src/bin/read.rs
@@ -2,10 +2,28 @@ extern crate parquet;
 
 use std::fs::File;
 use std::path::Path;
+use std::rc::Rc;
 
 use parquet::file::reader::{FileReader, SerializedFileReader};
 use parquet::schema::printer::print_file_metadata;
 use parquet::schema::parser::parse_message_type;
+use parquet::schema::types::Type;
+use parquet::record::api::{GroupConverter, RecordMaterializer};
+
+struct MyRecord {}
+
+impl MyRecord {}
+
+impl RecordMaterializer for MyRecord {
+  fn get_root_converter(&mut self, schema: &Type) -> &GroupConverter {
+    println!("creating root converter for schema: {:?}", schema);
+    unimplemented!();
+  }
+
+  fn consume_current_record(&mut self) {
+    println!("Consume current record");
+  }
+}
 
 fn main() {
     let path = Path::new("data/sample2.snappy.parquet");
@@ -20,5 +38,6 @@ fn main() {
       }
     ";
     let schema = parse_message_type(schema).unwrap();
-    parquet_reader.read_data(schema);
+    let recmat = Rc::new(MyRecord {});
+    parquet_reader.read_data(schema, recmat);
 }
---
+++ src/record/api.rs
@@ -0,0 +1,52 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use schema::types::{Type as SchemaType};
+use data_type::*;
+
+pub trait RecordMaterializer {
+  fn get_root_converter(&mut self, schema: &SchemaType) -> &GroupConverter;
+  fn consume_current_record(&mut self);
+}
+
+pub trait GroupConverter {
+  fn start(&mut self);
+
+  fn end(&mut self);
+
+  fn get_child_group_converter(&self, ordinal: usize) -> &GroupConverter;
+
+  fn get_child_primitive_converter(&self, ordinal: usize) -> &PrimitiveConverter;
+}
+
+pub trait PrimitiveConverter {
+  fn add_boolean(&mut self, value: bool);
+
+  fn add_int32(&mut self, value: i32);
+
+  fn add_int64(&mut self, value: i64);
+
+  fn add_int96(&mut self, value: Int96);
+
+  fn add_float(&mut self, value: f32);
+
+  fn add_double(&mut self, value: f64);
+
+  fn add_byte_array(&mut self, value: ByteArray);
+
+  fn add_fixed_len_byte_array(&mut self, value: ByteArray);
+}
