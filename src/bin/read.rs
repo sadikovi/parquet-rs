cf3bfb7d3763d4a9ec11d4fdc4030aa078775ad0
sadikovi <ivan.sadikov@gmail.com>
2018-03-11 12:26:04 +1300

    update read script

--- src/bin/read.rs
+++ src/bin/read.rs
@@ -1,14 +1,22 @@
 extern crate parquet;
 
+use std::env;
 use std::fs::File;
 use std::path::Path;
+use std::process;
 
 use parquet::file::reader::{FileReader, SerializedFileReader};
 use parquet::schema::printer::{print_file_metadata, print_schema};
 use parquet::schema::parser::parse_message_type;
 
 fn main() {
-  let path = Path::new("data/sample4.snappy.parquet");
+  let args: Vec<_> = env::args().collect();
+  if args.len() != 2 {
+    println!("Usage: read <file-path>");
+    process::exit(1);
+  }
+
+  let path = Path::new(&args[1]);
   let file = File::open(&path).unwrap();
   let parquet_reader = SerializedFileReader::new(file).unwrap();
   let metadata = parquet_reader.metadata();
